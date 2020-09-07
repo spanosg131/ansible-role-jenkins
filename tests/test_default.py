ff6ee7bc00072801d36723dabfa1aa6ab928d7ec
Georgios Spanos <spanosg@gmail.com>
2020-09-07 14:23:02 -0400

    modified default tests

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,17 +1,9 @@
+# Tests that apply to all systems go here
 import testinfra
 import pytest
+import os
+import testinfra.utils.ansible_runner
 
-@pytest.mark.parametrize("name,version", [
-    ("java-1.8.0-openjdk-devel",""),
-    ("initscripts",""),
-    ("git",""),
-    ("jenkins","")
-])
-def test_package_is_installed(host, name, version):
-    package = host.package(name)
-    assert package.is_installed
-
-def test_service_is_enabled_started(host):
-    jenkins_service = host.service("jenkins")
-    assert jenkins_service.is_running
-    assert jenkins_service.is_enabled
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    
