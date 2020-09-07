d5a771f14f583fb93bfae1121c6930fcf917528f
Georgios Spanos <spanosg@gmail.com>
2020-09-07 14:22:27 -0400

    added CentOS specific tests

---
+++ tests/test_centos.py
@@ -0,0 +1,23 @@
+# All CentOS specific tests go here
+import testinfra
+import pytest
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('ansible-role-jenkins-centos')
+
+@pytest.mark.parametrize("name,version", [
+    ("java-1.8.0-openjdk-devel",""),
+    ("initscripts",""),
+    ("git",""),
+    ("jenkins","")
+])
+def test_package_is_installed(host, name, version):
+    package = host.package(name)
+    assert package.is_installed
+
+def test_service_is_enabled_started(host):
+    jenkins_service = host.service("jenkins")
+    assert jenkins_service.is_running
+    assert jenkins_service.is_enabled
